41768da12d197b73a0b7067a923ba0fa7f80d94e
Vinay Prasad <vinayprasad89@gmail.com>
2015-10-26 22:59:43 -0700

    Basic function stuff 2

--- python_basics/functions.py
+++ python_basics/functions.py
@@ -20,3 +20,13 @@ greet_user_1()	# prints the default value specified
 def greet_user_2(user_name="VinayPrasad"):
 	print ("Hello, {}!".format(user_name))
 greet_user_2("R2-D2")	
+
+# A method can be passed as an arg to another method
+# This is because methods (and classes) are objects in python
+def method1():
+	return "from method1"
+
+def method2(some_method):
+	print (some_method())
+
+method2(method1)
