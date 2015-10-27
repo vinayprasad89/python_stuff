dfd1dcee97651dff17ebf592e4c1dcce7aa43b89
Vinay Prasad <vinayprasad89@gmail.com>
2015-10-26 22:42:55 -0700

    Basic math stuff

---
+++ python_basics/math_stuff.py
@@ -0,0 +1,26 @@
+# Will cover some basic math functions
+
+# Most of the basic math operations are obvious. The only thing to remember
+# is the exponentiation is marked with **
+exp = 2 ** 4  # 2 raised to the power 4
+print exp
+
+# Any operation that involves a floating point value, returns a floating point
+# result
+
+#################
+
+string_val = '5'
+int_val = 5
+result = string_val * int_val
+print result	# This prints 55555
+
+# Using int() to convert a string to an integer
+string_val = '5'
+int_val = 5
+result = int(string_val) * int_val
+print result
+
+# Similarly we can use the float method
+print float("24")
+print float(1)
